b84808660dff853d5cc1b18e98071a256a0f7446
ShannoonRuaaa <shannonruan@hotmail.com>
2023-09-19 10:51:24 -0700

    SVD

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import csv
+import numpy as np
 
 def convertToGraph(filepath):
     net = nx.MultiDiGraph()
@@ -14,3 +15,32 @@ def convertToGraph(filepath):
 
 net = convertToGraph("data\\edgesNoKey.csv")
 # print([i for i in net.neighbors(49724252)])
+
+# Define a matrix
+# A = np.array([[1, 2, 3],
+#               [4, 5, 6],
+#               [7, 8, 9]])
+
+A = np.array([[0.2, 0.1],
+             [0.8, 0.9]])
+
+B = np.array([[1,0],
+              [0,1]])
+
+
+C = np.subtract(A,B)
+print(C)
+D = [-C[0][1], C[0][0]];
+print(D)
+# Perform SVD
+U, S, V = np.linalg.svd(A)
+
+# U, S, and V are the factorized matrices
+print("U:")
+print(U)
+
+print("\nS:")
+print(S)
+
+print("\nV:")
+print(V)
