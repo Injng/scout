b2ab072367a3e6af3e75383611fddd128bbfdc76
BuildTools <unconfigured@null.spigotmc.org>
2023-09-18 22:30:13 -0700

    in method

--- converter.py
+++ converter.py
@@ -1,13 +1,16 @@
 import networkx as nx
 import csv
 
-net = nx.MultiDiGraph()
+def convertToGraph(filepath):
+    net = nx.MultiDiGraph()
+    with open(filepath, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            if row[6] != "Forbidden":
+                net.add_edge(int(row[2]), int(row[3]), float(row[4]), wkt=row[11], type=row[6])
+            if row[7] != "Forbidden":
+                net.add_edge(int(row[3]), int(row[2]), float(row[4]), wkt=row[11], type=row[7])
+    return net
 
-with open('data\\edgesNoKey.csv', newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    for row in csvreader:
-        if row[6] != "Forbidden":
-            net.add_edge(int(row[2]), int(row[3]), float(row[4]), wkt=row[11], type=row[6])
-        if row[7] != "Forbidden":
-            net.add_edge(int(row[3]), int(row[2]), float(row[4]), wkt=row[11], type=row[7])
+net = convertToGraph("data\\edgesNoKey.csv")
 # print([i for i in net.neighbors(49724252)])
