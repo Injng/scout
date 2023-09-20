1d441be5a060e77a54e71831259f193b1715c1c9
alex <alexw8015@gmail.com>
2023-09-20 10:17:14 -0700

    update from csv

--- converter.py
+++ converter.py
@@ -7,10 +7,9 @@ import requests
 
 stationsfinal = [49762207, 5445789476, 3011762637, 5294170126, 8455344314, 9748716629, 10109933697, 3669211313, 2146602922, 2146602922, 49283688, 49283688, 49283688, 2146602922, 2146602922, 49762191, 2791130921, 3000430332, 9083099465, 4401779099, 9068828613, 9082995718, 2591082909, 49752478, 49752478, 49752478, 9481540451, 49762207, 1381307999, 8626399984, 9123357154, 63349926, 575531863, 645731563, 643359029, 703037116, 703037116, 8901206658, 2057037354, 644150792, 644150792, 316162894, 5610652552, 6179193967, 49796861, 10087854710, 49792125, 9592649173, 1443170224, 49841238, 646246183, 1474688577, 4108851327, 4108851327, 4108851327, 3012784294, 5446812471, 8411953540, 2921245044, 1937256103, 9068828613, 50795293, 50795293, 50795293, 50795293, 49762191, 3012535014, 49796861, 3006162400, 8459177912, 4788602128, 5445925412, 49750033, 50483631, 50791672, 3660934424, 49769407, 649841051, 50490877, 50490877, 50490877, 316162894, 316162894, 63346822, 292915466, 292915466, 292915466, 63359657, 63359657, 63359657, 63359657, 6801446389, 6801446389, 6801446389, 6801446389, 49252020, 49252020, 49252020, 49252020, 49252020, 49252020, 49252020]
 # Get API key from environment variable API_KEY, requires a .env file in the same directory
-# API_KEY = os.getenv("API_KEY")
-API_KEY = "73d4a5d2f6fa4acb9a905bb0b14e073c"
+API_KEY = os.getenv("API_KEY")
 codes = ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B09', 'B10', 'B11', 'B35', 'C01', 'C02', 'C03', 'C04', 'C05', 'C06', 'C07', 'C08', 'C09', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'D01', 'D02', 'D03', 'D04', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11', 'D12', 'D13', 'E01', 'E02', 'E03', 'E04', 'E05', 'E06', 'E07', 'E08', 'E09', 'E10', 'F01', 'F02', 'F03', 'F04', 'F05', 'F06', 'F07', 'F08', 'F09', 'F10', 'F11', 'G01', 'G02', 'G03', 'G04', 'G05', 'J02', 'J03', 'K01', 'K02', 'K03', 'K04', 'K05', 'K06', 'K07', 'K08', 'N01', 'N02', 'N03', 'N04', 'N06', 'N07', 'N08', 'N09', 'N10', 'N11', 'N12']
-
+namesfinal = {'A01': 'Metro Center', 'A02': 'Farragut North', 'A03': 'Dupont Circle', 'A04': 'Woodley Park-Zoo/Adams Morgan', 'A05': 'Cleveland Park', 'A06': 'Van Ness-UDC', 'A07': 'Tenleytown-AU', 'A08': 'Friendship Heights', 'A09': 'Bethesda', 'A10': 'Medical Center', 'A11': 'Grosvenor-Strathmore', 'A12': 'North Bethesda', 'A13': 'Twinbrook', 'A14': 'Rockville', 'A15': 'Shady Grove', 'B01': 'Gallery Pl-Chinatown', 'B02': 'Judiciary Square', 'B03': 'Union Station', 'B04': 'Rhode Island Ave-Brentwood', 'B05': 'Brookland-CUA', 'B06': 'Fort Totten', 'B07': 'Takoma', 'B08': 'Silver Spring', 'B09': 'Forest Glen', 'B10': 'Wheaton', 'B11': 'Glenmont', 'B35': 'NoMa-Gallaudet U', 'C01': 'Metro Center', 'C02': 'McPherson Square', 'C03': 'Farragut West', 'C04': 'Foggy Bottom-GWU', 'C05': 'Rosslyn', 'C06': 'Arlington Cemetery', 'C07': 'Pentagon', 'C08': 'Pentagon City', 'C09': 'Crystal City', 'C10': 'Ronald Reagan Washington National Airport', 'C11': 'Potomac Yard', 'C12': 'Braddock Road', 'C13': 'King St-Old Town', 'C14': 'Eisenhower Avenue', 'C15': 'Huntington', 'D01': 'Federal Triangle', 'D02': 'Smithsonian', 'D03': "L'Enfant Plaza", 'D04': 'Federal Center SW', 'D05': 'Capitol South', 'D06': 'Eastern Market', 'D07': 'Potomac Ave', 'D08': 'Stadium-Armory', 'D09': 'Minnesota Ave', 'D10': 'Deanwood', 'D11': 'Cheverly', 'D12': 'Landover', 'D13': 'New Carrollton', 'E01': 'Mt Vernon Sq 7th St-Convention Center', 'E02': 'Shaw-Howard U', 'E03': 'U Street/African-Amer Civil War Memorial/Cardozo', 'E04': 'Columbia Heights', 'E05': 'Georgia Ave-Petworth', 'E06': 'Fort Totten', 'E07': 'West Hyattsville', 'E08': 'Hyattsville Crossing', 'E09': 'College Park-U of Md', 'E10': 'Greenbelt', 'F01': 'Gallery Pl-Chinatown', 'F02': 'Archives-Navy Memorial-Penn Quarter', 'F03': "L'Enfant Plaza", 'F04': 'Waterfront', 'F05': 'Navy Yard-Ballpark', 'F06': 'Anacostia', 'F07': 'Congress Heights', 'F08': 'Southern Avenue', 'F09': 'Naylor Road', 'F10': 'Suitland', 'F11': 'Branch Ave', 'G01': 'Benning Road', 'G02': 'Capitol Heights', 'G03': 'Addison Road-Seat Pleasant', 'G04': 'Morgan Boulevard', 'G05': 'Downtown Largo', 'J02': 'Van Dorn Street', 'J03': 'Franconia-Springfield', 'K01': 'Court House', 'K02': 'Clarendon', 'K03': 'Virginia Square-GMU', 'K04': 'Ballston-MU', 'K05': 'East Falls Church', 'K06': 'West Falls Church', 'K07': 'Dunn Loring-Merrifield', 'K08': 'Vienna/Fairfax-GMU', 'N01': 'McLean', 'N02': 'Tysons', 'N03': 'Greensboro', 'N04': 'Spring Hill', 'N06': 'Wiehle-Reston East', 'N07': 'Reston Town Center', 'N08': 'Herndon', 'N09': 'Innovation Center', 'N10': 'Washington Dulles International Airport', 'N11': 'Loudoun Gateway', 'N12': 'Ashburn'}
 def get_train(id):
     next_train = requests.get(f"https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{id}", headers={"api_key" : API_KEY})
 
@@ -42,21 +41,30 @@ def get_name(id):
     station_name = station_name.json()
     return station_name["Name"]
 
+def get_names():
+    out = {}
+    for code in codes:
+        out[code] = get_name(code)
+    return out
+
 def get_trains():
     out = {}
     for i in codes:
-        print(i)
+        # print(i)
         out[i] = len(get_train(i))
     return out
 
 def get_stats(day, AM_PM):
     stats = {}
-    trains = get_trains()
+    # trains = get_trains()
+    trains = {}
+    for code in codes:
+        trains[code] = 5
     need = need_for_each_station(day, AM_PM)
     for i in trains.keys():
-        print(i)
+        # print(i)
         singleneed = 0
-        name = get_name(i)
+        name = namesfinal[i]
         for j in need:
             if j[0] == name:
                 singleneed = j[1]
@@ -79,7 +87,7 @@ def get_shortestpaths(ubers, stations, net):
     totaldist = 0
     distances = {}
     paths = []
-    write_ubers(ubers)
+    write_ubers(ubers, net)
     for i in range(len(ubers)):
         print(i)
         uber = ubers[i]
@@ -99,7 +107,7 @@ def get_shortestpaths(ubers, stations, net):
                 for key in distances[uber].keys():
                     if distances[uber][key] == d[i]:
                         if stations[key] > 0:
-                            paths.append([uber, key])
+                            paths.append([uber, key, get_latlon(uber, net), get_latlon(key, net)])
                             totaldist += d[i]
                             stations[key] -= 1
                             l = False
@@ -113,12 +121,22 @@ def get_shortestpathstime(ubers, day, AM_PM, div, net):
         stations[stationsfinal[codes.index(i)]] = stats[i] // div
     return get_shortestpaths(ubers, stations, net)
 
-def write_ubers(ubers):
-    with open('data\\ubers.csv', 'w') as file:
+def update(day, AM_PM, div, net):
+    ubers = []
+    with open("data/ubers.csv", newline='\n') as file:
+        print(reader)
+        for row in reader:
+            if row != ["uber_id","lat","lon"]:
+                ubers.append(get_node(float(row[1]), float(row[2]), net))
+    return get_shortestpathstime(ubers, day, AM_PM, div, net)
+
+def write_ubers(ubers, net):
+    with open('data/ubers.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(["uber_id","uber_node"])
+        writer.writerow(["uber_id","lat","lon"])
         for i in range(len(ubers)):
-            writer.writerow([i, ubers[i]])
+            latlon = get_latlon(ubers[i], net)
+            writer.writerow([i, latlon[0], latlon[1]])
 
 def dist(x1, y1, x2, y2):
     return math.sqrt((x2-x1) ** 2 + (y2-y1) ** 2)
@@ -231,9 +249,11 @@ def need_for_each_station(day, AM_PM):#the day is a string monday, tuesday ... t
 # print(get_stats('monday','PM Peak (3pm-7pm)'))
 
 net = convert_to_graph("data\\edgesNoKey.csv")
-# print(get_shortestpathstime(get_random_ubers(120, net), 'monday', 'PM Peak (3pm-7pm)', net))
+print(get_shortestpathstime(get_random_ubers(1, net), 'wednesday', 'PM Peak (3pm-7pm)', 10, net))
+print(update('wednesday', 'PM Peak (3pm-7pm)', 10, net))
 
-write_ubers(get_random_ubers(120, net))
+# print(get_names())
+# write_ubers(get_random_ubers(120, net))
 
 # print(net.nodes)
 # print(nx.dijkstra_path_length(net, 10127575312, 10092398257, weight="w"))
