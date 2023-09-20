fdf1eb36e01f6b5c6b53d3e7b31ca66a83192881
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 22:19:46 -0700

    made get_node

--- converter.py
+++ converter.py
@@ -1,27 +1,28 @@
 import networkx as nx
 import csv
 import numpy as np
+import math
 
-def convertToGraph(filepath):
+def convert_to_graph(filepath):
     net = nx.MultiDiGraph()
     with open(filepath, newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         for row in csvreader:
             if row[6] != "Forbidden":
-                net.add_edge(int(row[2]), int(row[3]), w=float(row[4]), wkt=row[11], type=row[6])
+                net.add_edge(int(row[2]), int(row[3]), w=float(row[4]), wkt=row[11], type=row[6], dir=1)
             if row[7] != "Forbidden":
-                net.add_edge(int(row[3]), int(row[2]), w=float(row[4]), wkt=row[11], type=row[7])
+                net.add_edge(int(row[3]), int(row[2]), w=float(row[4]), wkt=row[11], type=row[7], dir=-1)
     return net
 
 # takes a list of ubers and a dictionary of stations with the node id being the key and the value being the number of ubers needed
-def getShortestPaths(ubers, stations, net):
+def get_shortestpaths(ubers, stations, net):
 	distances = {}
 	paths = []
 	for uber in ubers:
-		uberDistances = {}
+		uber_distances = {}
 		for station in stations.keys():
-			uberDistances[station] = nx.dijkstra_path_length(net, uber, station, weight="w")
-		distances[uber] = uberDistances
+			uber_distances[station] = nx.dijkstra_path_length(net, uber, station, weight="w")
+		distances[uber] = uber_distances
 	for uber in ubers:
 		d = [distances[uber][i] for i in distances[uber].keys()]
 		d.sort()
@@ -37,16 +38,50 @@ def getShortestPaths(ubers, stations, net):
 						break
 	return paths
 
-def getNode(lat, long):
-	return 0
+def dist(x1, y1, x2, y2):
+	return math.sqrt((x2-x1) ** 2 + (y2-y1) ** 2)
 
-net = convertToGraph("data\\edgesNoKey.csv")
+def get_latlon(node, net):
+	try:
+		edge = list(net.out_edges(node))[0]
+		edge += (0,)
+		con = net.edges[edge]
+		wkt = con["wkt"]
+		spl = wkt.split(" ")
+		if con["dir"] == 1:
+			return [float(spl[0][11:]), float(spl[1][:len(spl[1])-1])]
+		if con["dir"] == -1:
+			return [float(spl[len(spl)-2]), float(spl[len(spl)-1][:len(spl[len(spl)-1])-1])]
+	except:
+		edge = list(net.in_edges(node))[0]
+		edge += (0,)
+		con = net.edges[edge]
+		wkt = con["wkt"]
+		spl = wkt.split(" ")
+		if con["dir"] == 1:
+			return [float(spl[0][11:]), float(spl[1][:len(spl[1])-1])]
+		if con["dir"] == -1:
+			return [float(spl[len(spl)-2]), float(spl[len(spl)-1][:len(spl[len(spl)-1])-1])]
+
+def get_node(lat, lon, net):
+	lowest_dist = -1
+	right = -1
+	for node in net.nodes:
+		latlon = get_latlon(node, net)
+		d = dist(lat, lon, latlon[0], latlon[1])
+		if lowest_dist == -1 or d < lowest_dist:
+			lowest_dist = d
+			right = node
+	return right
+
+net = convert_to_graph("data\\edgesNoKey.csv")
 # print(net.nodes)
-print(nx.dijkstra_path_length(net, 10127575312, 10092398257, weight="w"))
-print(getShortestPaths([10127575312,10127575312,10127575312,4788602122, 4643843108, 49751367, 10092398257, 10979910220], {10070536261: 3, 10127575312:3}, net))
+# print(nx.dijkstra_path_length(net, 10127575312, 10092398257, weight="w"))
+# print(getShortestPaths([10127575312,10127575312,10127575312,4788602122, 4643843108, 49751367, 10092398257, 10979910220], {10070536261: 3, 10127575312:3}, net))
 # print([i for i in net.neighbors(49724252)])
-
-
+# print(get_latlon(6799231643, net))
+print(get_node(-77.0545830,38.9853795, net))
+print(get_latlon(get_node(-77.0545830,38.9853795, net), net))
 
 
 # Define a matrix
