4705aa8e003c5bc8bfa67434bf629769c04b1f52
Injng <jlinjiang@gmail.com>
2023-09-20 16:37:36 -0700

    Working proof of concept

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import csv
+from converter import update_one
 
 app = Flask(__name__,template_folder="templates")
 
@@ -11,18 +12,10 @@ def hello():
 def location():
     lat = request.form.get("data")
     lon = request.form.get("data2")
-    with open('data/ubers.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(["uber_id", "lat", "lon"])
-        writer.writerow([1, lat, lon]) 
-    return lat + lon
-
-def write_ubers(ubers):
-    with open('data/ubers.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(["uber_id","lat", "lon"])
-        for i in range(len(ubers)):
-            writer.writerow([i, ubers[i]])
+    print("call")
+    optimal = update_one(lat, lon, 'wednesday', 'PM Peak (3pm-7pm)', 10)[3]
+    print(optimal)
+    return str(optimal[0]) + ':' + str(optimal[1])
 
 if __name__ == '__main__':
     app.run(debug=True)
